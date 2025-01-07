bfa9bc32f8771941d7685b920f11ff2f0e6e3164
Daniel Dihardja <daniel@dihardja.de>
2025-01-07 17:44:39 +0100

    add tailwind

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -1,9 +1,7 @@
-import NxWelcome from '../nx-welcome';
-
 export default function Index() {
   return (
     <div>
-      <NxWelcome title={'web'} />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
