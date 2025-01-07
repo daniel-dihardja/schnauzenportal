945d59cb4c131090842ad884d3894e126b5c7ee1
Daniel Dihardja <daniel@dihardja.de>
2025-01-07 13:56:44 +0100

    add default api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json()); // For parsing JSON payloads
 app.use(express.urlencoded({ extended: true })); // For parsing URL-encoded payloads
 
+app.get('/', (req, res) => {
+  res.send('Schnauzenportal');
+});
+
 app.post('/search', async (req, res) => {
   const message = req.body.message;
   const msg = new HumanMessage({ content: message });
