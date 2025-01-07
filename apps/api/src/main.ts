5dd07e3a5d77da49a4d6fca4df80de9f5e7553f4
Daniel Dihardja <daniel@dihardja.de>
2025-01-07 13:15:56 +0100

    add agent api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import { HumanMessage } from '@langchain/core/messages';
 import express from 'express';
-import * as path from 'path';
+import { graph } from './agent';
 
 const app = express();
+app.use(express.json()); // For parsing JSON payloads
+app.use(express.urlencoded({ extended: true })); // For parsing URL-encoded payloads
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.post('/search', async (req, res) => {
+  const message = req.body.message;
+  const msg = new HumanMessage({ content: message });
+  const result = await graph.invoke({ messages: [msg] });
+  res.json(result);
 });
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
