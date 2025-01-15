cdaf749d7d41a04885e40a95c8a944dada683bbb
Daniel Dihardja <daniel@dihardja.de>
2025-01-15 18:23:59 +0100

    create search page

--- apps/web/app/routes/_layout.search.tsx
+++ apps/web/app/routes/_layout.search.tsx
@@ -34,7 +34,7 @@ interface FetcherData {
   individualPetAnswers?: Pet[];
 }
 
-export default function Index() {
+export default function Search() {
   const fetcher = useFetcher<FetcherData>();
   const [message, setMessage] = useState('');
   const [isButtonEnabled, setIsButtonEnabled] = useState(false);
@@ -47,54 +47,52 @@ export default function Index() {
   const pets = fetcher.data?.individualPetAnswers || [];
 
   return (
-    <div className="mx-auto px-2 max-w-[1024px] mt-8">
-      <fetcher.Form method="post">
-        <div className="grid grid-cols-1 gap-4">
-          <style>
-            {`
-                #petDescription::placeholder {
-                color: #BCC0C6; /* Custom color between gray-300 and gray-400 */
-              }
-              `}
-          </style>
-          <Textarea
-            label="Schreiben Sie hier, wonach Sie suchen:"
-            id="pet-description"
-            name="message"
-            placeholder="Beispiel: „Ich suche einen Hund, der möglicherweise Verhaltensprobleme hat oder als schwierig gilt. Ich habe Erfahrung mit solchen Hunden, keine Kinder im Haushalt und bin bereit, mit ihm zu arbeiten.“"
-            fullWidth={true}
-            size="lg"
-            minRows={3}
-            onChange={(e) => setMessage(e.target.value)}
-            variant="flat"
-            description="Beschreiben Sie das gesuchte Haustier und Ihre Wohnsituation, z. B. „eine aktive Katze auf dem Land neben einem Wald“."
-            classNames={{
-              label: 'text-lg font-bold px-2',
-              input: 'p-2',
-              description: 'text-center text-gray-400 mt-2',
-            }}
-          />
-        </div>
-        <div className="flex justify-center">
-          <Button
-            type="submit"
-            className={`w-full md:w-64 p-6 mt-4 bg-black text-white ${
-              !isButtonEnabled
-                ? 'opacity-30 cursor-not-allowed'
-                : 'hover:bg-gray-800'
-            }`}
-            style={!isButtonEnabled ? { pointerEvents: 'none' } : {}}
-            disabled={!isButtonEnabled}
-            isLoading={fetcher.state === 'submitting'}
-          >
-            <strong>Passende Haustiere finden</strong>
-          </Button>
-        </div>
-      </fetcher.Form>
+    <fetcher.Form method="post" className="mx-auto px-2 max-w-[1024px] mt-8">
+      <div className="grid grid-cols-1 gap-4">
+        <style>
+          {`
+            #petDescription::placeholder {
+              color: #BCC0C6;
+            }
+          `}
+        </style>
+        <Textarea
+          label="Schreiben Sie hier, wonach Sie suchen:"
+          id="pet-description"
+          name="message"
+          placeholder="Beispiel: „Ich suche einen Hund, der möglicherweise Verhaltensprobleme hat oder als schwierig gilt. Ich habe Erfahrung mit solchen Hunden, keine Kinder im Haushalt und bin bereit, mit ihm zu arbeiten.“"
+          fullWidth={true}
+          size="lg"
+          minRows={3}
+          onChange={(e) => setMessage(e.target.value)}
+          variant="flat"
+          description="Beschreiben Sie das gesuchte Haustier und Ihre Wohnsituation, z. B. „eine aktive Katze auf dem Land neben einem Wald“."
+          classNames={{
+            label: 'text-lg font-bold px-2',
+            input: 'p-2',
+            description: 'text-center text-gray-400 mt-2',
+          }}
+        />
+      </div>
+      <div className="flex justify-center">
+        <Button
+          type="submit"
+          className={`w-full md:w-64 p-6 mt-4 bg-black text-white ${
+            !isButtonEnabled
+              ? 'opacity-30 cursor-not-allowed'
+              : 'hover:bg-gray-800'
+          }`}
+          style={!isButtonEnabled ? { pointerEvents: 'none' } : {}}
+          disabled={!isButtonEnabled}
+          isLoading={fetcher.state === 'submitting'}
+        >
+          <strong>Passende Haustiere finden</strong>
+        </Button>
+      </div>
 
-      {pets ? (
+      {pets.length > 0 && (
         <div>
-          {<p className="mt-8 text-large">{summary}</p>}
+          {summary && <p className="mt-8 text-large">{summary}</p>}
           <div className="columns-1 md:columns-2 gap-4 mt-6">
             {pets.map((pet: Pet, index: number) => (
               <Card key={index} className="mb-4">
@@ -112,11 +110,12 @@ export default function Index() {
                         <strong>{pet.id}</strong>
                       </h3>
                       <p>{pet.answer}</p>
-                      <p className=" pt-2">
+                      <p className="pt-2">
                         <a
                           className="text-blue-600 underline"
                           href={pet.url}
                           target="_blank"
+                          rel="noreferrer"
                         >
                           {pet.url}
                         </a>
@@ -128,7 +127,7 @@ export default function Index() {
             ))}
           </div>
         </div>
-      ) : null}
-    </div>
+      )}
+    </fetcher.Form>
   );
 }
